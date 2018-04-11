d573a9fe773778b6a5602703bcb3cb6a98ce230d
nathandunk <nbd2@rice.edu>
2018-04-10 21:51:50 -0500

    Plot is created with coordinate frames at each link

--- functions.py
+++ functions.py
@@ -1,21 +1,95 @@
-from numpy import *
+import numpy as np
+import sympy as sym
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
 
-class robot(rp_vector):
+# Define the robot class
+class robot:
 
-    robot.size = length(rp_vector)
-    for i in rp_vector:
-        robot.add_joint(i)
+    def __init__(self,rp_vector):
+        self.rp_vector      = rp_vector
+        self.size           = len(rp_vector)
+        self.joints         = ["0"]*self.size
+        self.alpha          = np.zeros(self.size)
+        self.a              = np.zeros(self.size)
+        self.d              = np.zeros(self.size)
+        self.theta          = np.zeros(self.size)
+        self.T              = np.zeros((4,4,self.size))
+        self.T_full         = np.zeros((4,4))
+        self.joint_values   = np.zeros(self.size)
 
-    def add_joint(joint_type):
-        if joint_type == r:
+        self.O              = np.zeros((3,self.size+1))
 
+        self.X              = np.zeros((3,self.size+1))
+        self.Y              = np.zeros((3,self.size+1))
+        self.Z              = np.zeros((3,self.size+1))
 
-def dhtf(alpha, a, d, theta):
-    T = np.matrix([[np.cos(theta),        -sin(theta),           0,           a],...
-                  [sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d],...
-                  [sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha),  cos(alpha)*d],...
-                  [0,                     0,                     0,           1]])
-    return T
+        for i in self.rp_vector:
+            self.add_joint(i)
+
+    def add_joint(self,joint_type):
+        if joint_type == 'r':
+            self.joints.append("revolute")
+        else:
+            self.joints.append("prismatic")
+
+    def dh(self,alpha_,a_,d_,theta_):
+        self.alpha = alpha_
+        self.a = a_
+        self.d = d_
+        self.theta = theta_
+        # self.dhtf(self.alpha,self.a,self.d,self.theta)
+
+    def dhtf(self, alpha, a, d, theta):
+
+        # theta.subs(theta1,joint_values(1))
+        # theta.subs(theta2,joint_values(2))
+        # theta.subs(theta3,joint_values(3))
+
+        self.T_full = np.identity(4)
+        for i in range(0,self.size+1):
+            T_hold = np.array([[np.cos(theta[i]),    -np.sin(theta[i]),                   0,                 a[i]],
+                [np.sin(theta[i])*np.cos(alpha[i]), np.cos(theta[i])*np.cos(alpha[i]), -np.sin(alpha[i]), -np.sin(alpha[i])*d[i]],
+                [np.sin(theta[i])*np.sin(alpha[i]), np.cos(theta[i])*np.sin(alpha[i]),  np.cos(alpha[i]),  np.cos(alpha[i])*d[i]],
+                [0,                                 0,                                  0,                 1]])
+            # print(T_hold.shape)
+            # print(T_hold)
+            self.T_full = np.matmul(self.T_full,T_hold)
+
+            self.O[:,i] = self.T_full[0:3,3]
+
+            print(self.O[:,i])
+            print(self.T_full[0:3,0])
+            self.X[:,i] = self.O[:,i]+self.T_full[0:3,0]*2
+            self.Y[:,i] = self.O[:,i]+self.T_full[0:3,1]*2
+            self.Z[:,i] = self.O[:,i]+self.T_full[0:3,2]*2
+            # print(self.T_full)
+
+def create_3D_plot():
+    pg.mkQApp()
+
+    ## make a widget for displaying 3D objects
+    view = gl.GLViewWidget()
+    view.show()
+
+    ## create three grids, add each to the view
+    xgrid = gl.GLGridItem()
+    ygrid = gl.GLGridItem()
+    zgrid = gl.GLGridItem()
+    view.addItem(xgrid)
+    view.addItem(ygrid)
+    view.addItem(zgrid)
+
+    # ## rotate x and y grids to face the correct direction
+    xgrid.rotate(90, 0, 1, 0)
+    ygrid.rotate(90, 1, 0, 0)
+
+    ## scale each grid differently
+    xgrid.scale(1, 1, 1)
+    ygrid.scale(1, 1, 1)
+    zgrid.scale(1, 1, 1)
+
+    return view
 
 # def draw_axis(location,joint_angles):
 
--- main.py
+++ main.py
@@ -1,55 +1,57 @@
 import pyqtgraph as pg
 import sys
 import pyqtgraph.opengl as gl
-import functions
+from functions import *
 import numpy as np
-import pyqtgraph.opengl as gl
+import time
+import sympy as s
 
 if __name__ == "__main__":
 
-    a = []
+    theta1, theta2, theta3 = sym.symbols('theta1 theta2 theta3')
+    l1 = 20
+    l2 = 30
+    # link            1    2         3
+    alpha = np.array([0,        np.pi/2,  0,      0])
+    a     = np.array([0,        0,        l1,     l2])
+    d     = np.array([0,        0,        0,      0])
+    theta = np.array([theta1,   theta2,   theta3, 0])
+    my_robot = robot('rrr')
 
-    pg.mkQApp()
+    my_robot.dh(alpha,a,d,theta)
 
-    ## make a widget for displaying 3D objects
-    view = gl.GLViewWidget()
-    view.show()
+    my_robot.joint_values = [0,np.pi/2,np.pi/4, 0]
+    my_robot.dhtf(my_robot.alpha,my_robot.a,my_robot.d,my_robot.joint_values)
 
-    ## create three grids, add each to the view
-    xgrid = gl.GLGridItem()
-    ygrid = gl.GLGridItem()
-    zgrid = gl.GLGridItem()
-    view.addItem(xgrid)
-    view.addItem(ygrid)
-    view.addItem(zgrid)
+    # print(my_robot.T_full)
+    view = create_3D_plot()
 
-    # ## rotate x and y grids to face the correct direction
-    xgrid.rotate(90, 0, 1, 0)
-    ygrid.rotate(90, 1, 0, 0)
+    p = np.array([my_robot.O[0,:],my_robot.O[1,:],my_robot.O[2,:]]).transpose()
+    x_base = np.array([[0,3],[0,0],[0,0]]).transpose()
+    y_base = np.array([[0,0],[0,3],[0,0]]).transpose()
+    z_base = np.array([[0,0],[0,0],[0,3]]).transpose()
+    # p = p.transpose()
 
-    ## scale each grid differently
-    xgrid.scale(1, 1, 1)
-    ygrid.scale(1, 1, 1)
-    zgrid.scale(1, 1, 1)
+    C = pg.glColor('w')
+    R = pg.glColor('r')
+    G = pg.glColor('g')
+    
+    B = pg.glColor('b')
 
-    X  = np.linspace(-10,10,100)
-    Y1 = 2+np.sin(X)
-    Y2 = -2+Y1*Y1
-    Z  = np.linspace(-10,10,100)
-    p  = np.array([X,Y2,Z])
+    plt = gl.GLLinePlotItem(pos=p, width=3, color=C)
 
-    p = p.transpose() 
+    for i in range(0,my_robot.size+1):
+        x_unit = np.array([my_robot.O[:,i],my_robot.X[:,i]])
+        y_unit = np.array([my_robot.O[:,i],my_robot.Y[:,i]])
+        z_unit = np.array([my_robot.O[:,i],my_robot.Z[:,i]])
+        pltx = gl.GLLinePlotItem(pos = x_unit, width = 5, color = R)
+        plty = gl.GLLinePlotItem(pos = y_unit, width = 5, color = G)
+        pltz = gl.GLLinePlotItem(pos = z_unit, width = 5, color = B)
+        view.addItem(pltx)
+        view.addItem(plty)
+        view.addItem(pltz)
 
-    C=pg.glColor('w')
-
-    plt = gl.GLLinePlotItem(pos=p, width=2, color=C)
-
-
-    view.addItem(plt)  ## setting pen=None disables line drawing
-
-    ## Switch to using white background and black foreground
-    # pg.setConfigOption('background', 'w')
-    # pg.setConfigOption('foreground', 'k')
+    view.addItem(plt)
 
     if sys.flags.interactive != 1 or not hasattr(QtCore, 'PYQT_VERSION'):
         pg.QtGui.QApplication.exec_()
